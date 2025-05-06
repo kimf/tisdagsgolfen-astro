c81715fe32f1904574465642ccd46fa266378e68
Kim Fransman <kim.fransman@daresay.co>
2025-05-06 11:10:17 +0200

    Some PWA work and styling etc

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -15,9 +15,14 @@ export const STORAGE_THEME_KEY = 'theme' as const;
 export const themeStore = persistentAtom<ThemeValue>(STORAGE_THEME_KEY, THEME_MAP.light);
 
 const init = () => {
-  themeStore.subscribe((theme) =>
-    document.documentElement.classList[theme === THEME_MAP.dark ? 'add' : 'remove'](THEME_MAP.dark)
-  );
+  themeStore.subscribe((theme) => {
+    document.documentElement.classList[theme === THEME_MAP.dark ? 'add' : 'remove'](THEME_MAP.dark);
+    const metaTag = document.querySelector('meta[name=theme-color]');
+    metaTag?.setAttribute(
+      'content',
+      theme === THEME_MAP.dark ? 'oklch(0.141 0.005 285.823)' : '#f3f3f398'
+    );
+  });
   return themeStore.off();
 };
 
