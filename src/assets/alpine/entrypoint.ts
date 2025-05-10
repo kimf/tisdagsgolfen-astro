be82c1fabc9ad316d80e688249d0e827f4d7e83d
Kim Fransman <kim.fransman@daresay.co>
2025-05-10 16:44:40 +0200

    Brought back back links, removed Astro transitions and fixed ActiveSession

--- src/assets/alpine/entrypoint.ts
+++ src/assets/alpine/entrypoint.ts
@@ -1,12 +1,5 @@
-import Alpine from 'alpinejs';
 import type { Alpine as AlpineType } from 'alpinejs';
 import AutoAnimate from '@marcreichel/alpine-auto-animate';
-
-if (import.meta.hot) {
-  import.meta.hot.accept(() => {
-    console.log('HMR');
-  });
-}
 
 export default (Alpine: AlpineType) => {
   Alpine.plugin(AutoAnimate);
